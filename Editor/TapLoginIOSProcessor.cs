873023c7dbd7a02246c9d1b80a4fe151c433aa71
xiaoyi <xiaoyi@xd.com>
2021-07-06 15:53:36 +0800

    feat(*): Bootstrap remove unuse interface , remove native library , iOS compilation script migration to login

--- Editor/TapLoginIOSProcessor.cs
+++ Editor/TapLoginIOSProcessor.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using TapTap.Common.Editor;
 using UnityEditor;
 using UnityEditor.Callbacks;
@@ -11,14 +12,27 @@ namespace TapTap.Login.Editor
         public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
         {
             if (buildTarget != BuildTarget.iOS) return;
-            
+
+            var parentFolder = Directory.GetParent(Application.dataPath).FullName;
+
+            var plistFile = TapFileHelper.RecursionFilterFile(parentFolder + "/Assets/Plugins/", "TDS-Info.plist");
+
+            if (!plistFile.Exists)
+            {
+                Debug.LogError("TapLogin Can't find TDS-Info.plist in Project/Assets/Plugins/!");
+            }
+
+            TapCommonCompile.HandlerPlist(path, plistFile.FullName);
+
+            // UnityAppController.mm 中对于 URLScheme 的处理
             var unityAppControllerPath = path + "/Classes/UnityAppController.mm";
             var unityAppController = new TapFileHelper(unityAppControllerPath);
             unityAppController.WriteBelow(@"#import <OpenGLES/ES2/glext.h>", @"#import <TapLoginSDK/TapLoginHelper.h>");
             unityAppController.WriteBelow(
                 @"id sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey], annotation = options[UIApplicationOpenURLOptionsAnnotationKey];",
                 @"if(url){[TapLoginHelper handleTapTapOpenURL:url];}");
-            Debug.Log("TapLogin Change AppControler File!");
+
+            Debug.Log("TapLogin Change AppController File!");
         }
     }
 }
