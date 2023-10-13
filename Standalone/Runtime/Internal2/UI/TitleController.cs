46392d8957596aa5dcf8c6774b47f999d399acd9
ci-gitlab <tds-developer@xd.com>
2023-10-13 20:34:06 +0800

    feat:update upm

---
+++ Standalone/Runtime/Internal2/UI/TitleController.cs
@@ -0,0 +1,23 @@
+﻿using TapTap.Login.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleController {
+    private readonly Text leftText;
+    private readonly Image logoImage;
+    private readonly Text rightText;
+
+    public TitleController(Transform transform) {
+        leftText = transform.Find("LeftText").GetComponent<Text>();
+        logoImage = transform.Find("Logo").GetComponent<Image>();
+        rightText = transform.Find("RightText").GetComponent<Text>();
+    }
+
+    public void Load() {
+        ILoginLang lang = LoginLanguage.GetCurrentLang();
+        leftText.text = lang.TitleUse();
+        leftText.gameObject.SetActive(!string.IsNullOrEmpty(leftText.text));
+        rightText.text = lang.TitleLogin();
+        rightText.gameObject.SetActive(!string.IsNullOrEmpty(rightText.text));
+    }
+}
